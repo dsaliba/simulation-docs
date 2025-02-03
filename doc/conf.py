557339e292938906de2978a5207ee01bed21f9e4
Dimitri Saliba <dimitriasaliba@gmail.com>
2025-02-03 11:11:14 -0500

    Enviornment Building sectrion started

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinxsharp.sphinxsharp']
+extensions = ['myst_parser', 'sphinxsharp.sphinxsharp', 'sphinx.ext.autosectionlabel']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
